f64e3760af45fa95d74a023fe503f8fce78e7580
hurui <hu.rui0530@gmail.com>
2023-07-02 11:00:45 +0800

    my webui backup initialization

--- webui-user.sh
+++ webui-user.sh
@@ -9,38 +9,42 @@
 # Name of the subdirectory
 #clone_dir="stable-diffusion-webui"
 
+# Fix tensorflow warning
+export TF_ENABLE_ONEDNN_OPTS=0
+
 # Commandline arguments for webui.py, for example: export COMMANDLINE_ARGS="--medvram --opt-split-attention"
-#export COMMANDLINE_ARGS=""
+export COMMANDLINE_ARGS="--opt-channelslast --no-half --share --enable-insecure-extension-access --xformers --disable-safe-unpickle"
 
 # python3 executable
-#python_cmd="python3"
+python_cmd="python"
 
 # git executable
-#export GIT="git"
+# export GIT="git"
 
 # python3 venv without trailing slash (defaults to ${install_dir}/${clone_dir}/venv)
-#venv_dir="venv"
+# venv_dir="venv"
 
 # script to launch to start the app
-#export LAUNCH_SCRIPT="launch.py"
+export LAUNCH_SCRIPT="launch.py"
 
 # install command for torch
-#export TORCH_COMMAND="pip install torch==1.12.1+cu113 --extra-index-url https://download.pytorch.org/whl/cu113"
+# export TORCH_COMMAND="pip install torch==1.12.1+cu113 --extra-index-url https://download.pytorch.org/whl/cu113"
 
 # Requirements file to use for stable-diffusion-webui
-#export REQS_FILE="requirements_versions.txt"
+# export REQS_FILE="requirements_versions.txt"
 
 # Fixed git repos
-#export K_DIFFUSION_PACKAGE=""
-#export GFPGAN_PACKAGE=""
+# export K_DIFFUSION_PACKAGE=""
+# export GFPGAN_PACKAGE=""
 
 # Fixed git commits
-#export STABLE_DIFFUSION_COMMIT_HASH=""
-#export TAMING_TRANSFORMERS_COMMIT_HASH=""
-#export CODEFORMER_COMMIT_HASH=""
-#export BLIP_COMMIT_HASH=""
+# export STABLE_DIFFUSION_COMMIT_HASH=""
+# export TAMING_TRANSFORMERS_COMMIT_HASH=""
+# export CODEFORMER_COMMIT_HASH=""
+# export BLIP_COMMIT_HASH=""
 
 # Uncomment to enable accelerated launch
-#export ACCELERATE="True"
+# export ACCELERATE="True"
 
 ###########################################
+python launch.py
