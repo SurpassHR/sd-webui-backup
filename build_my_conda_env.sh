6208708e7747d4e1bdfcafe68e8b0dfd10b18505
surpasshr <hu.rui0530@gmail.com>
2023-07-09 16:32:55 +0800

    build env script for seetacloud

--- build_my_conda_env.sh
+++ build_my_conda_env.sh
@@ -1,21 +1,48 @@
-# 创建 conda python3.10.6环境
-conda create -n newEnv python=3.10.6 -y
-# 激活新环境
-conda activate newEnv
-# 查看 python 是否安装成功
-python3.10 --version | grep "Python" && echo "Install OK" || echo "Install Failed"
-# 安装依赖
-python3.10 -m pip install -U xformers --root-user-action=ignore
-# 安装 pytorch1.13.1+cu117
-!python3.10 -m pip install /kaggle/input/torch1131-cu117/torch-1.13.1%2Bcu117-cp310-cp310-linux_x86_64.whl --root-user-action=ignore -q > /dev/null 2>&1
-# 清理缓存垃圾
-!python3.10 -m pip cache purge -q > /dev/null 2>&1
+# 执行前将id_rsa.pub添加到huggingface仓库公钥
+set SD_WEBUI_ROOT=/root/autodl-tmp/sd-webui
 # 克隆项目
-git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui /root/autodl-tmp/sd-webui
-# 克隆插件
-git clone https://github.com/hako-mikan/sd-webui-supermerger                   # superMerger
-git clone https://github.com/AlUlkesh/stable-diffusion-webui-images-browser    # image browser
-git clone https://github.com/kohya-ss/sd-webui-additional-networks             # Lora network
-git clone https://github.com/dtlnor/stable-diffusion-webui-localization-zh_CN  # localization
-git clone https://github.com/DominikDoom/a1111-sd-webui-tagcomplete            # tag compelete
-git clone https://github.com/a2569875/lora-prompt-tool                         # Lora prompt tool
+git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui ${SD_WEBUI_ROOT}
+# 配置变量
+set VENV_ROOT=${SD_WEBUI_ROOT}/venv
+set MODEL_ROOT=${SD_WEBUI_ROOT}/models
+set MODEL_DIR=${MODEL_ROOT}/Stable-diffusion
+set LORA_DIR=${MODEL_ROOT}/Lora
+set VAE_DIR=${MODEL_ROOT}/VAE
+set SWINIR_DIR=${MODEL_ROOT}/SwinIR
+set CODEFORMER_GIT_DIR=${SD_WEBUI_ROOT}/repositories/CodeFormer
+set FACELIB_WEIGHT_DIR=${CODEFORMER_GIT_DIR}/weights/facelib
+set CODEFORMER_WEIGHT_DIR=${CODEFORMER_GIT_DIR}/weights/CodeFormer
+set FACELIB_DIR=${MODEL_ROOT}/facelib
+set TURBO=/etc/network_turbo
+
+set LORA_HF_STORE_GIT=git@hf.co:SurpassHR/LoraBackup
+
+# 安装venv
+apt install venv
+# 安装git-lfs
+apt install git-lfs
+# 写入命令行参数
+set FIRST_START_ARGS="--exit --xformers"
+set DEFAULT_START_ARGS="--share --xformers --enable-insecure-extension-access --lora-dir ${LORA_DIR}"
+# 第一次启动自动执行安装虚拟环境并退出
+source ${TURBO} && ${SD_WEBUI_ROOT}/launch.py ${FIRST_START_ARGS}
+# 下载必要插件
+git clone https://github.com/hako-mikan/sd-webui-supermerger
+git clone https://github.com/AlUlkesh/stable-diffusion-webui-images-browser
+git clone https://github.com/kohya-ss/sd-webui-additional-networks
+git clone https://github.com/dtlnor/stable-diffusion-webui-localization-zh_CN
+git clone https://github.com/DominikDoom/a1111-sd-webui-tagcomplete
+git clone https://github.com/a2569875/lora-prompt-tool
+# 下载必要模型
+source ${TURBO} && wget https://huggingface.co/SurpassHR/ModelBackup/resolve/FantasticMix/FantasticMix.safetensors -O ${MODEL_DIR}/FantasticMix.safetensors
+source ${TURBO} && wget https://huggingface.co/SurpassHR/ModelBackup/resolve/Gape/Gape60.safetensors -O ${MODEL_DIR}/Gape60.safetensors
+# 下载必要Lora
+git clone ${LORA_HF_STORE_GIT} ${LORA_DIR} && cd ${LORA_DIR} && git lfs pull && rm -rf .git && cd -
+# 下载必要VAE
+wget https://huggingface.co/stabilityai/sd-vae-ft-mse-original/resolve/main/vae-ft-mse-840000-ema-pruned.ckpt -O ${VAE_DIR}/vae-ft-mse-840000-ema-pruned.ckpt
+# 下载SwinIR
+wget https://github.com/JingyunLiang/SwinIR/releases/download/v0.0/003_realSR_BSRGAN_DFOWMFC_s64w8_SwinIR-L_x4_GAN.pth -O ${SWINIR_DIR}/SwinIR_4x.pth
+# 下载脸部修复Coderformer/facelib
+wget "https://github.com/sczhou/CodeFormer/releases/download/v0.1.0/detection_Resnet50_Final.pth" -O ${FACELIB_WEIGHT_DIR}/detection_Resnet50_Final.pth
+wget "https://github.com/sczhou/CodeFormer/releases/download/v0.1.0/parsing_parsenet.pth" -O ${FACELIB_WEIGHT_DIR}/parsing_parsenet.pth
+wget "https://github.com/sczhou/CodeFormer/releases/download/v0.1.0/codeformer.pth" -O ${CODEFORMER_WEIGHT_DIR}/codeformer.pth
